f80d076b2b36229f13e7f84902d788096c6ddc1b
sagar <sagar.varat@mindnerves.com>
2021-06-24 13:48:38 +0530

    combining 3 to 1
    test
    
    test 123

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	custmarshal "github.com/sagarvarat/practice-git/custommarshal"
 )
 
 // This is comment
 func main() {
-	fmt.Println("Hello Git from main....")
+	fmt.Println("Hello Git from task1233")
+	custmarshal.CustomMarshal()
 }
